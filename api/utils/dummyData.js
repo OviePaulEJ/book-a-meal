2389fc5faf6004fae599b6ee6008f1db2c1487fc
oviepaulej <oviepe@gmail.com>
2019-02-18 19:08:21 +0100

    create meal service

---
+++ api/utils/dummyData.js
@@ -0,0 +1,28 @@
+export default {
+    meals: {
+        {
+            id: 1,
+            name: 'Ofada Rice',
+            size: 'Medium',
+            price: '500',
+        }
+        {
+            id: 1,
+            name: 'Jollof Rice',
+            size: 'Small',
+            price: '300',
+        }
+        {
+            id: 1,
+            name: 'Beans',
+            size: 'Large',
+            price: '300',
+        }
+        {
+            id: 1,
+            name: 'Salad',
+            size: 'Small',
+            price: '200',
+        }
+    }
+}
